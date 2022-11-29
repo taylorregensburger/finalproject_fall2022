a16526a6b106dd526d978c06547d1ec98e2af78a
nickbertel <117771937+nickbertel@users.noreply.github.com>
2022-11-29 16:11:54 -0500

    Update finalprojgraphing.py

--- finalprojgraphing.py
+++ finalprojgraphing.py
@@ -4,8 +4,9 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 tickerstring = str(input("Input a ticker"))
+periodlength = str(input("Input a length of time for the graph(1yr,6mo,2wk,5d")
 tick = yf.Ticker(tickerstring)
-tickhist = tick.history(period="6mo")
+tickhist = tick.history(period=periodlength)
 tickhist['Close'].plot(title="apple stock price")
 plt.show()
 
